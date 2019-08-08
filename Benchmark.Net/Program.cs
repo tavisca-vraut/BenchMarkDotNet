8ca9a098f55f5a9385897bc9c801067ac8a701ed
vighnesh153 <vighnesh.raut13@gmail.com>
2019-08-08 14:43:45 +0530

    Add tasks vs thread benchmark

--- Benchmark.Net/Program.cs
+++ Benchmark.Net/Program.cs
@@ -32,7 +32,8 @@ namespace Benchmark.Net
 
             //BenchmarkRunner.Run<StringVsStringBuilder>();
             //BenchmarkRunner.Run<ForVsWhileLoop>();
-            BenchmarkRunner.Run<IfVsSwitch>();
+            //BenchmarkRunner.Run<IfVsSwitch>();
+            BenchmarkRunner.Run<TaskVsThread>();
             Console.ReadKey(true);
         }
     }
---
+++ Benchmark.Net/TaskVsThread.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Benchmark.Net
+{
+    public class TaskVsThread
+    {
+        public int loopCount = 100;
+        public void SimpleLoop()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                
+            }
+        }
+
+        [Benchmark]
+        public void Threads()
+        {
+            var thread = new Thread(SimpleLoop);
+
+            thread.Start();
+        }
+
+        [Benchmark]
+        public void Tasks()
+        {
+            var task = new Task(SimpleLoop);
+            task.Start();
+        }
+
+        [Benchmark]
+        public void TaskFactory()
+        {
+            Task.Factory.StartNew(SimpleLoop);
+        }
+
+        [Benchmark]
+        public void ThreadPools()
+        {
+            ThreadPool.QueueUserWorkItem(o => SimpleLoop());
+        }
+    }
+}
