070b0fac13ff011274df9af6727bf7087459f393
vighnesh153 <vighnesh.raut13@gmail.com>
2019-08-08 10:27:43 +0530

    initial commit

---
+++ Benchmark.Net/CsvValueReader.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+using CsvHelper;
+using System.IO;
+using System.Text;
+
+namespace Benchmark.Net
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+
+            return $"{Name}: {Age}";
+        }
+    }
+    public class CsvValueReader
+    {
+        public static string filePath = @"C:\TaviscaDev\Benchmark.Net\Benchmark.Net\testset.csv";
+
+        [Benchmark]
+        public void StringConcatenator()
+        {
+            string s = string.Empty;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csvFile = new CsvReader(reader))
+                {
+                    while (csvFile.Read())
+                    {
+                        s += csvFile.GetRecord<Person>().ToString();
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        public void StringBuilder()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csvFile = new CsvReader(reader))
+                {
+                    while (csvFile.Read())
+                    {
+                        stringBuilder.Append(csvFile.GetRecord<Person>().ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Benchmark.Net/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Running;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Benchmark.Net
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            //List<Person> nameAge = new List<Person>();
+
+            //Random random = new Random();
+
+            //foreach (var i in Enumerable.Range(0, 10000))
+            //{
+            //    nameAge.Add(new Person { Name = "Vighnesh", Age = random.Next(20, 80) });
+            //}
+
+            //using (TextWriter writer = new StreamWriter(@"C:\TaviscaDev\Benchmark.Net\Benchmark.Net\testset.csv", false, System.Text.Encoding.UTF8))
+            //{
+            //    using (var csv = new CsvWriter(writer))
+            //    {
+            //        csv.WriteRecords(nameAge); // where values implements IEnumerable
+            //    }
+            //}
+
+            BenchmarkRunner.Run<CsvValueReader>();
+            Console.ReadKey(true);
+        }
+    }
+}
